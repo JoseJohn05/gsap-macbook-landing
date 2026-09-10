5a12e42239f4feb9475711608ead2485e84b14dd
JoseJohn05 <jsampwaya@gmail.com>
2026-09-10 16:28:32 +0200

    Implement 3D Product Viewer Section

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -9,11 +9,28 @@ Title: macbook pro M3 16 inch 2024
 */
 
 import {useGLTF, useTexture} from '@react-three/drei'
+import useMacBookStore from "../../store/index.js";
+import {noChangeParts} from "../../constants/index.js";
+import {useEffect} from "react";
+import {Color} from "three";
 
 export default function MacBookModel14(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-14-transformed.glb');
+    const {color} = useMacBookStore();
+  const { nodes, materials, scene} = useGLTF('/models/macbook-14-transformed.glb');
 
-  const texture = useTexture(('/screen.png'))
+  const texture = useTexture(('/screen.png'));
+
+    useEffect(() => {
+        scene.traverse((child) => {
+            if(child.isMesh){
+                //change color only if the part name is NOT noChangeParts
+                if(!noChangeParts.includes(child.name)){
+                    child.material.color = new Color(color);
+                }
+            }
+        })
+    }, [color,scene]);
+
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -9,11 +9,27 @@ Title: macbook pro M3 16 inch 2024
 */
 
 import {useGLTF, useTexture} from '@react-three/drei'
+import {useEffect} from "react";
+import {noChangeParts} from "../../constants/index.js";
+import {Color, Scene} from "three";
+import useMacBookStore from "../../store/index.js";
 
-export function Model(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-16-transformed.glb')
+export default function MacBookModel16(props) {
+    const {color} = useMacBookStore();
+  const { nodes, materials, scene } = useGLTF('/models/macbook-16-transformed.glb')
 
-    const texture = useTexture('/screen.png')
+    const texture = useTexture('/screen.png');
+
+    useEffect(() => {
+        scene.traverse((child) => {
+            if(child.isMesh){
+                //change color only if the part name is NOT noChangeParts
+                if(!noChangeParts.includes(child.name)){
+                    child.material.color = new Color(color);
+                }
+            }
+        })
+    }, [color, scene]);
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
